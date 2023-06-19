0d85dbdf34fc152c325ddcfc0d14c678cfe5d424
mirellioli16 <106408365+mirellioli16@users.noreply.github.com>
2023-06-19 20:01:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+
+
+function titulo() {
+    var titulo = window.document.getElementById('titulo')
+    var resp_ti = prompt('Insira o título: ')
+    titulo.innerText = resp_ti
+}
+
+function subtitulo() {
+    var sub = window.document.getElementById('subtitulo')
+    var resp_subt = prompt('Insira um Subtítulo: ')
+    sub.innerText = resp_subt
+}
+
+function descricao() {
+    var desc = window.document.getElementById('descricao')
+    var resp_desc = prompt('Digite a descrição: ')
+    desc.innerText = resp_desc
+}
+
+function itens1() {
+    var itens1 = window.document.getElementById('itens1')
+    var resp_itens = prompt('insira o itens1: ')
+    itens1.innerText = resp_itens
+}
+
+function itens2() {
+    var itens1 = window.document.getElementById('itens2')
+    var resp_itens = prompt('insira o itens2: ')
+    itens1.innerText = resp_itens
+}
+
+function itens3() {
+    var itens1 = window.document.getElementById('itens3')
+    var resp_itens = prompt('insira o itens3: ')
+    itens1.innerText = resp_itens
+}
+
+function itens4() {
+    var itens1 = window.document.getElementById('itens4')
+    var resp_itens = prompt('insira o itens4: ')
+    itens1.innerText = resp_itens
+}
+
+function itens5() {
+    var itens1 = window.document.getElementById('itens5')
+    var resp_itens = prompt('insira o itens5: ')
+    itens1.innerText = resp_itens
+}
+
+function itens6() {
+    var itens1 = window.document.getElementById('itens6')
+    var resp_itens = prompt('insira o itens6: ')
+    itens1.innerText = resp_itens
+}
+
+function itens7() {
+    var itens1 = window.document.getElementById('itens7')
+    var resp_itens = prompt('insira o itens7: ')
+    itens1.innerText = resp_itens
+}
+
+function itens8() {
+    var itens1 = window.document.getElementById('itens8')
+    var resp_itens = prompt('insira o itens8: ')
+    itens1.innerText = resp_itens
+}
+
+
+
+
+
+
+titulo(), subtitulo(), descricao(), itens1(), itens2(), itens3(), itens4(), itens5(), itens6(), itens7(), itens8()
+
+
+
+
